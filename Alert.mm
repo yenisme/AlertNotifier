099aafff0dd6c26ab390f853893bbb4b09453997
Khổng Mạnh Yên <yenvoiu@gmail.com>
2025-08-20 19:55:13 +0700

    Add files via upload

--- Alert.mm
+++ Alert.mm
@@ -1,31 +1,52 @@
 #import <UIKit/UIKit.h>
+#import "FLAnimatedImage.h"
+#import "FLAnimatedImageView.h"
 
 __attribute__((constructor))
 static void showAlertAfterLaunch() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Khổng Mạnh Yên"
-                                                                       message:@"Inbox thì cứ thêm vài từ"Mình sẽ trả phí"là được😆"
-                                                                preferredStyle:UIAlertControllerStyleAlert];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC),
+                   dispatch_get_main_queue(), ^{
+
+        UIAlertController *alert =
+        [UIAlertController alertControllerWithTitle:@"Khổng Mạnh Yên"
+                                            message:@"Inbox thì cứ thêm vài từ \"Mình sẽ trả phí\" là được 😆"
+                                     preferredStyle:UIAlertControllerStyleAlert];
+
+        // ====== Load GIF ======
+        NSString *gifPath = @"/Library/Application Support/AlertNotifier/yen.gif";
+        NSData *gifData = [NSData dataWithContentsOfFile:gifPath];
+        if (gifData) {
+            FLAnimatedImage *gifImage = [FLAnimatedImage animatedImageWithGIFData:gifData];
+            FLAnimatedImageView *gifView = [[FLAnimatedImageView alloc] init];
+            gifView.animatedImage = gifImage;
+            gifView.frame = CGRectMake(20, 15, 40, 40); // 40x40 ~ bằng tiêu đề
+            gifView.contentMode = UIViewContentModeScaleAspectFit;
+
+            [alert.view addSubview:gifView];
+        }
+
+        // ====== Nút Đóng ======
         UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Đóng"
                                                      style:UIAlertActionStyleDefault
                                                    handler:nil];
         [ok setValue:[UIColor redColor] forKey:@"titleTextColor"];
         [alert addAction:ok];
+
+        // ====== Nút Website ======
         UIAlertAction *openLink = [UIAlertAction actionWithTitle:@"Website"
                                                            style:UIAlertActionStyleDefault
                                                          handler:^(UIAlertAction * _Nonnull action) {
             NSURL *url = [NSURL URLWithString:@"https://manhyen.github.io/bio/"];
             if ([[UIApplication sharedApplication] canOpenURL:url]) {
-                [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+                [[UIApplication sharedApplication] openURL:url options:@{}
+                                         completionHandler:nil];
             }
         }];
         [alert addAction:openLink];
-        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert
-                                                                                     animated:YES
-                                                                                   completion:nil];
+
+        // Hiển thị Alert
+        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+        UIViewController *rootVC = keyWindow.rootViewController;
+        [rootVC presentViewController:alert animated:YES completion:nil];
     });
 }
-
-
-    
-
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-export THEOS=/Users/dothanh1110/theos
-ARCHS = arm64 
+export THEOS=/home/codespace/theos
+ARCHS = arm64
 
 DEBUG = 0
 FINALPACKAGE = 1
@@ -8,10 +8,15 @@ FOR_RELEASE = 1
 include $(THEOS)/makefiles/common.mk
 
 TWEAK_NAME = Alert
-$(TWEAK_NAME)_FRAMEWORKS =  UIKit
+
+$(TWEAK_NAME)_FRAMEWORKS = UIKit
 $(TWEAK_NAME)_CCFLAGS = -std=c++11 -fno-rtti -fno-exceptions -DNDEBUG
-$(TWEAK_NAME)_CFLAGS = -fobjc-arc 
-$(TWEAK_NAME)_FILES = Alert.mm
+$(TWEAK_NAME)_CFLAGS = -fobjc-arc
+
+# Thêm FLAnimatedImage vào danh sách file
+$(TWEAK_NAME)_FILES = \
+    Alert.mm \
+    Vendor/FLAnimatedImage/FLAnimatedImage.m \
+    Vendor/FLAnimatedImage/FLAnimatedImageView.m
 
 include $(THEOS_MAKE_PATH)/tweak.mk
-
