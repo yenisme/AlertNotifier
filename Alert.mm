1cc34a9e55e9bce58537966a1f11445ae854d522
Khổng Mạnh Yên <yenvoiu@gmail.com>
2025-08-21 15:05:35 +0700

    Update Alert.mm

--- Alert.mm
+++ Alert.mm
@@ -2,20 +2,28 @@
 #import "FLAnimatedImage.h"
 #import "FLAnimatedImageView.h"
 #import "FPSDisplay.h"
+
 __attribute__((constructor))
 static void showAlertAfterLaunch() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC),
                    dispatch_get_main_queue(), ^{
-                   [[FPSDisplay sharedInstance] start];
+        // Bật overlay FPS
+        [[FPSDisplay sharedInstance] start];
+
+        // ====== Tạo Alert ======
         UIAlertController *alert =
         [UIAlertController alertControllerWithTitle:@"Khổng Mạnh Yên"
                                             message:@"Inbox thì cứ thêm vài từ \"Mình sẽ trả phí\" là được 😆"
                                      preferredStyle:UIAlertControllerStyleAlert];
-// ====== Tiêu đề xanh lá ======
-NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:@"Khổng Mạnh Yên"];
-[title addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(0, title.length)];
-[title addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:18] range:NSMakeRange(0, title.length)];
-[alert setValue:title forKey:@"attributedTitle"];
+
+        // ====== Đổi màu tiêu đề ======
+        NSMutableAttributedString *titleAttr =
+        [[NSMutableAttributedString alloc] initWithString:@"Khổng Mạnh Yên"];
+        [titleAttr addAttribute:NSForegroundColorAttributeName
+                          value:[UIColor systemGreenColor]
+                          range:NSMakeRange(0, titleAttr.length)];
+        [alert setValue:titleAttr forKey:@"attributedTitle"];
+
         // ====== Load GIF ======
         NSString *gifPath = @"/Library/Application Support/AlertNotifier/yen.gif";
         NSData *gifData = [NSData dataWithContentsOfFile:gifPath];
@@ -23,7 +31,7 @@ NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithSt
             FLAnimatedImage *gifImage = [FLAnimatedImage animatedImageWithGIFData:gifData];
             FLAnimatedImageView *gifView = [[FLAnimatedImageView alloc] init];
             gifView.animatedImage = gifImage;
-            gifView.frame = CGRectMake(20, 15, 40, 40); // 40x40 ~ bằng tiêu đề
+            gifView.frame = CGRectMake(20, 15, 40, 40); // 40x40
             gifView.contentMode = UIViewContentModeScaleAspectFit;
 
             [alert.view addSubview:gifView];
@@ -42,15 +50,15 @@ NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithSt
                                                          handler:^(UIAlertAction * _Nonnull action) {
             NSURL *url = [NSURL URLWithString:@"https://manhyen.github.io/bio/"];
             if ([[UIApplication sharedApplication] canOpenURL:url]) {
-                [[UIApplication sharedApplication] openURL:url options:@{}
+                [[UIApplication sharedApplication] openURL:url
+                                                   options:@{}
                                          completionHandler:nil];
             }
         }];
-        //nút Website
-[openLink setValue:[UIColor systemPinkColor] forKey:@"titleTextColor"];
+        [openLink setValue:[UIColor systemPinkColor] forKey:@"titleTextColor"];
         [alert addAction:openLink];
 
-        // Hiển thị Alert
+        // ====== Hiển thị Alert ======
         UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
         UIViewController *rootVC = keyWindow.rootViewController;
         [rootVC presentViewController:alert animated:YES completion:nil];
